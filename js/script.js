8811e1d0d28af01f035ca690034d0f3f5d8e6eef
D_Sykes <Itachi_15_90@yahoo.com>
2024-02-21 21:49:38 -0600

    changed 8 from string to a number

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ const updateGuestCount = function (){
     const guests = document.querySelectorAll(".guest-list li");
     guestCount.innerText = guests.length;
 
-    if (guests.length == "8"){
+    if (guests.length == 8){
         addGuestButton.classList.add("hide");
         guestInput.classList.add("hide");
         guestInputLabel.classList.add("hide");
